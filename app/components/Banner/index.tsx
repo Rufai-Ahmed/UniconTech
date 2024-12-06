d7188e509ce8363f1c4a149fb6924a86affc99b3
DEV-Topper <rehobothekene@gmail.com>
2024-12-06 01:05:35 +0100

    Add

--- app/components/Banner/index.tsx
+++ app/components/Banner/index.tsx
@@ -5,7 +5,6 @@ const Banner = () => {
     <div className="mx-auto max-w-7xl my-10 sm:py-10 px-6 lg:px-8">
       <div className="grid grid-cols-1 lg:grid-cols-2 my-16">
         {/* COLUMN-1 */}
-
         <div className="mx-auto sm:mx-0">
           <div className="py-3 text-center lg:text-start">
             <button className="text-blue bg-lightblue hover:shadow-xl text-sm md:text-lg font-bold px-6 py-1 rounded-3xl tracking-wider hover:text-white hover:bg-black">
@@ -18,14 +17,19 @@ const Banner = () => {
             </h1>
           </div>
           <div className="my-7 text-center lg:text-start">
-            <button className="text-sm md:text-xl font-semibold hover:shadow-xl bg-blue text-white py-3 px-6 md:py-5 md:px-14 rounded-full hover:bg-hoblue">
-              Get Started
-            </button>
+            <a
+              href="https://wa.me/2349110212491"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <button className="text-sm md:text-xl font-semibold hover:shadow-xl bg-blue text-white py-3 px-6 md:py-5 md:px-14 rounded-full hover:bg-hoblue">
+                Get Started
+              </button>
+            </a>
           </div>
         </div>
 
         {/* COLUMN-2 */}
-
         <div className="lg:-m-24 lg:pt-20 hidden lg:block">
           <Image
             src="/images/banner/banner.svg"
